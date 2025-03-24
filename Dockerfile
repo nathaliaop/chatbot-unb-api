537139ee37d3999c9e38930dedfbb270236b75a6
Nathalia Pereira <np.nathaliapereira@gmail.com>
2025-03-23 21:29:03 -0300

    feat: Add prometheus-fastapi-instrumentator

--- Dockerfile
+++ Dockerfile
@@ -20,8 +20,8 @@ RUN pip install uvicorn fastapi qdrant-client openai dotenv logging pydantic typ
 
 RUN pip install prometheus-fastapi-instrumentator
 
-COPY . /app 
+COPY . /app
 
 EXPOSE 3000
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "3000", "--workers", "3"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "3000"]
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from qdrant_client import QdrantClient
 from dotenv import load_dotenv
 import os
@@ -7,6 +8,7 @@ from pydantic import BaseModel
 from typing import List
 from sentence_transformers import SentenceTransformer
 from openai import OpenAI
+import time
 from prometheus_fastapi_instrumentator import Instrumentator
 
 class Message(BaseModel):
@@ -18,43 +20,52 @@ class ChatRequest(BaseModel):
     messages: List[Message]
     temperature: float = 0.7
 
+# environment variables
 load_dotenv()
 
 # general
-MODEL_NAME = os.getenv("MODEL_NAME", "mistralai/Mistral-7B-Instruct-v0.2")
-MAX_TOKENS = int(os.getenv("MAX_TOKENS", 500))
+MODEL = os.getenv("MODEL")
 CHAT_TEMPLATE = os.getenv("CHAT_TEMPLATE", "Você é um chatbot da Universidade de Brasília feito para responder perguntas sobre assuntos relacionados a universidade. Responda a mensagem do usuário em português utilizando o contexto como base. \n\nContexto: {context}.\n\nUsuário: {user_message}")
 
-# OpenAI client
+# HuggingFace
 HUGGING_FACE_API_KEY = os.getenv("HUGGING_FACE_API_KEY")
-OPENAI_CLIENT_BASE_URL = os.getenv("OPENAI_CLIENT_BASE_URL")
+HUGGING_FACE_BASE_URL = os.getenv("HUGGING_FACE_BASE_URL")
+HUGGING_FACE_MODEL_NAME = os.getenv("HUGGING_FACE_MODEL_NAME")
+HUGGING_FACE_MAX_TOKENS = int(os.getenv("HUGGING_FACE_MAX_TOKENS", 500))
+
+# DeepSeek
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+DEEPSEEK_BASE_URL = os.getenv("DEEPSEEK_BASE_URL", "https://api.deepseek.com/v1")
+DEEPSEEK_MODEL_NAME = os.getenv("DEEPSEEK_MODEL_NAME", "deepseek-chat")
+DEEPSEEK_MAX_TOKENS = int(os.getenv("DEEPSEEK_MAX_TOKENS", 500))
 
 # Qdrant
 QDRANT_CLIENT_URL = os.getenv("QDRANT_CLIENT_URL")
 QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
 QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME")
-QDRANT_SEARCH_LIMIT = os.getenv("QDRANT_SEARCH_LIMIT")
+QDRANT_SEARCH_LIMIT = int(os.getenv("QDRANT_SEARCH_LIMIT", 5))
+
+env_vars = {
+    "MODEL": MODEL,
+    "HUGGING_FACE_API_KEY": HUGGING_FACE_API_KEY,
+    "HUGGING_FACE_BASE_URL": HUGGING_FACE_BASE_URL,
+    "HUGGING_FACE_MODEL_NAME": HUGGING_FACE_MODEL_NAME,
+    "DEEPSEEK_API_KEY": DEEPSEEK_API_KEY,
+    "DEEPSEEK_BASE_URL": DEEPSEEK_BASE_URL,
+    "DEEPSEEK_MODEL_NAME": DEEPSEEK_MODEL_NAME,
+    "QDRANT_CLIENT_URL": QDRANT_CLIENT_URL,
+    "QDRANT_API_KEY": QDRANT_API_KEY,
+    "QDRANT_COLLECTION_NAME": QDRANT_COLLECTION_NAME
+}
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-if HUGGING_FACE_API_KEY is None:
-    logger.warning("HUGGING_FACE_API_KEY environment variable is not defined.")
+for var_name, var_value in env_vars.items():
+    if not var_value:
+        logger.warning(f"{var_name} environment variable is not defined.")
 
-if OPENAI_CLIENT_BASE_URL is None:
-    logger.warning("OPENAI_CLIENT_BASE_URL environment variable is not defined.")
-
-if QDRANT_CLIENT_URL is None:
-    logger.warning("QDRANT_CLIENT_URL environment variable is not defined.")
-
-if QDRANT_API_KEY is None:
-    logger.warning("QDRANT_API_KEY environment variable is not defined.")
-
-if QDRANT_COLLECTION_NAME is None:
-    logger.warning("QDRANT_COLLECTION_NAME environment variable is not defined.")
-
-if QDRANT_SEARCH_LIMIT is None:
-    logger.warning("QDRANT_SEARCH_LIMIT environment variable is not defined.")
+# clients
 
 qclient = QdrantClient(
     url=QDRANT_CLIENT_URL,
@@ -64,7 +75,25 @@ qclient = QdrantClient(
 
 encoder = SentenceTransformer("all-MiniLM-L12-v2")
 
+deepseek_client = OpenAI(
+    base_url=DEEPSEEK_BASE_URL,
+    api_key=DEEPSEEK_API_KEY,
+)
+
+hugging_face_client = OpenAI(
+    base_url=HUGGING_FACE_BASE_URL,
+    api_key=HUGGING_FACE_API_KEY,
+)
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Instrumentator().instrument(app).expose(app)
 
@@ -88,8 +117,18 @@ async def status():
 @app.post("/v1/chat/completions")
 async def chat_completion(request: ChatRequest):
     user_message = request.messages[-1].content
+
+    start_time = time.time()
+
     context = get_context_from_qdrant(user_message)
-    
+
+    end_time = time.time()
+    elapsed_time1 = end_time - start_time
+
+    logger.info(f"get_context_from_qdrant: {elapsed_time1:.4f} seconds")
+
+    # messages = request.messages[-10:] # TODO: this as an environment variable
+
     messages = [
         {
             "role": "user",
@@ -97,18 +136,32 @@ async def chat_completion(request: ChatRequest):
         }
     ]
 
-    client = OpenAI(
-        base_url=OPENAI_CLIENT_BASE_URL,
-        api_key=HUGGING_FACE_API_KEY,
-    )
+    start_time = time.time()
 
-    completion = client.chat.completions.create(
-        model=MODEL_NAME, 
-        messages=messages, 
-        max_tokens=MAX_TOKENS,
-    )
+    if MODEL == 'deepseek':
+        completion = deepseek_client.chat.completions.create(
+            model=DEEPSEEK_MODEL_NAME, 
+            messages=messages, 
+            max_tokens=DEEPSEEK_MAX_TOKENS,
+            stream=False
+        )
+    elif MODEL == 'mistralai/Mistral-7B-Instruct-v0.2':
+        completion = hugging_face_client.chat.completions.create(
+            model=HUGGING_FACE_MODEL_NAME, 
+            messages=messages, 
+            max_tokens=HUGGING_FACE_MAX_TOKENS,
+            stream=False
+        )
+    else: # log on error
+        raise HTTPException(status_code=404, detail="Model not found")
 
-    logger.info(f'\nPrompt: {user_message}\nAnswer: {completion.choices[0].message.content}\n\n')
-    logger.info(f'{completion}')
+    end_time = time.time()
+    elapsed_time2 = end_time - start_time
+
+    logger.info(f"Completion: {elapsed_time2:.4f} seconds")
+    logger.info(f"Total: {(elapsed_time1 + elapsed_time2):.4f} seconds")
+
+    #logger.info(f'\nModel: {MODEL}\nPrompt: {user_message}\nAnswer: {completion.choices[0].message.content}\n\n')
+    #logger.info(f'{completion}')
 
     return completion
